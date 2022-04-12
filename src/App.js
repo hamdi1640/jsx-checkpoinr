c3036ce36c2553f4f2b8e3454017f7c9ba10b978
hamdi1640 <mahamdi732@gmail.com>
2022-04-12 05:23:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+<h1 class="title red">Your name here</h1>
+
+
+
+<img alt=""src="https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/hfpqyV7B-IMG-Dubai-UAE.jpg"></img>
+
+
+
+<img alt="" src="https://cdn.wallpapersafari.com/58/56/LwCyP6.jpg"></img>
+
+
+
+<video width="320" height="240" controls>
+</video>
+<video  src="https://www.youtube.com/watch?v=lLu4YGCjDww" type="video/mp4" >
+
+</video>
+    </div>
+    
+  );
+}
+
+export default App;
